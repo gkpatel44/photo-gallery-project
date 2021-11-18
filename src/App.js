4a48e3cb202db749703688721d377206105dc6d4
gaurang kathiriya <kathiriyagaurang44@gmail.com>
2021-11-18 20:17:21 +0530

    froent end completed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
 
 import './App.css';
+import  { useEffect, useState,} from 'react'
 
-function App() {
+const App = () => {
+  const [data, setData] = useState([])
+  useEffect(() => {
+  fetchdata();
+  }, [])
+
+  const fetchdata = async ()=>{
+    await fetch(`https://reqres.in/api/users?page=2`)
+    .then(resp=>resp.json())
+    .then(data=>{
+    console.log(data.data)
+    setData(data.data)})
+  }
   return (
-    <div className="App">
-    <p>helloapp</p>
+    <div className="container" >
+      <h1>Photos Gallery</h1>
+      {data.map((dataa)=>
+
+         
+     <img 
+     src={dataa.avatar}
+      />)}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
+
